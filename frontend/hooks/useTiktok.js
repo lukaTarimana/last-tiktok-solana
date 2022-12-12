e28423284a22b769e11b808b7aa23cb472ebaf48
lukaTarimana <104419299+lukaTarimana@users.noreply.github.com>
2022-12-12 16:22:02 +0400

    changes

--- frontend/hooks/useTiktok.js
+++ frontend/hooks/useTiktok.js
@@ -1,17 +1,18 @@
-import { TOKEN_PROGRAM_ID } from '@solana/spl-token'
-import { useWallet } from '@solana/wallet-adapter-react'
-import { SOLANA_HOST } from '../utils/const'
-import { getProgramInstance } from '../utils/utils'
-const anchor = require('@project-serum/anchor')
-const utf8 = anchor.utils.bytes.utf8
-const { BN, web3 } = anchor
-const { SystemProgram } = web3
+import { TOKEN_PROGRAM_ID } from "@solana/spl-token";
+import { useWallet } from "@solana/wallet-adapter-react";
+import { SOLANA_HOST } from "../utils/const";
+import { getProgramInstance } from "../utils/utils";
+import axios from "axios";
+const anchor = require("@project-serum/anchor");
+const utf8 = anchor.utils.bytes.utf8;
+const { BN, web3 } = anchor;
+const { SystemProgram } = web3;
 
 const defaultAccounts = {
   tokenProgram: TOKEN_PROGRAM_ID,
   clock: anchor.web3.SYSVAR_CLOCK_PUBKEY,
   systemProgram: SystemProgram.programId,
-}
+};
 
 const useTiktok = (
   setTikToks,
@@ -20,30 +21,30 @@ const useTiktok = (
   description,
   setDescription,
   setVideoUrl,
-  setNewVideoShow,
+  setNewVideoShow
 ) => {
-  const wallet = useWallet()
-  const connection = new anchor.web3.Connection(SOLANA_HOST)
-  const program = getProgramInstance(connection, wallet)
+  const wallet = useWallet();
+  const connection = new anchor.web3.Connection(SOLANA_HOST);
+  const program = getProgramInstance(connection, wallet);
   const getTiktoks = async () => {
-    console.log('fetching')
+    console.log("fetching");
 
-    const videos = await program.account.videoAccount.all()
-    console.log(videos)
+    // const videos = await program.account.videoAccount.all()
+    // console.log(videos)
 
-    // const res = await axios.get(
-    //   'https://ipfs.io/ipfs/QmS28E89P3Gz2LZimkKSuJgXGuZEtXG6dhyzxkSbpv6mKU/tiktoks.json',
-    // )
-    // setTikToks(res.data);
+    const res = await axios.get(
+      "https://ipfs.io/ipfs/QmS28E89P3Gz2LZimkKSuJgXGuZEtXG6dhyzxkSbpv6mKU/tiktoks.json"
+    );
+    setTikToks(res.data);
 
-    setTikToks(videos)
-  }
+    // setTikToks(res);
+  };
 
-  const likeVideo = async index => {
+  const likeVideo = async (index) => {
     let [video_pda] = await anchor.web3.PublicKey.findProgramAddress(
-      [utf8.encode('video'), new BN(index).toArrayLike(Buffer, 'be', 8)],
-      program.programId,
-    )
+      [utf8.encode("video"), new BN(index).toArrayLike(Buffer, "be", 8)],
+      program.programId
+    );
 
     const tx = await program.rpc.likeVideo({
       accounts: {
@@ -51,25 +52,25 @@ const useTiktok = (
         authority: wallet.publicKey,
         ...defaultAccounts,
       },
-    })
+    });
 
-    console.log(tx)
-  }
+    console.log(tx);
+  };
 
   const createComment = async (index, count, comment) => {
     let [video_pda] = await anchor.web3.PublicKey.findProgramAddress(
-      [utf8.encode('video'), new BN(index).toArrayLike(Buffer, 'be', 8)],
-      program.programId,
-    )
+      [utf8.encode("video"), new BN(index).toArrayLike(Buffer, "be", 8)],
+      program.programId
+    );
 
     let [comment_pda] = await anchor.web3.PublicKey.findProgramAddress(
       [
-        utf8.encode('comment'),
-        new BN(index).toArrayLike(Buffer, 'be', 8),
-        new BN(count).toArrayLike(Buffer, 'be', 8),
+        utf8.encode("comment"),
+        new BN(index).toArrayLike(Buffer, "be", 8),
+        new BN(count).toArrayLike(Buffer, "be", 8),
       ],
-      program.programId,
-    )
+      program.programId
+    );
 
     if (userDetail) {
       const tx = await program.rpc.createComment(
@@ -83,24 +84,24 @@ const useTiktok = (
             authority: wallet.publicKey,
             ...defaultAccounts,
           },
-        },
-      )
-      console.log(tx)
+        }
+      );
+      console.log(tx);
     }
-  }
+  };
 
   const newVideo = async () => {
     let [state_pda] = await anchor.web3.PublicKey.findProgramAddress(
-      [utf8.encode('state')],
-      program.programId,
-    )
+      [utf8.encode("state")],
+      program.programId
+    );
 
-    const stateInfo = await program.account.stateAccount.fetch(state_pda)
+    const stateInfo = await program.account.stateAccount.fetch(state_pda);
 
     let [video_pda] = await anchor.web3.PublicKey.findProgramAddress(
-      [utf8.encode('video'), stateInfo.videoCount.toArrayLike(Buffer, 'be', 8)],
-      program.programId,
-    )
+      [utf8.encode("video"), stateInfo.videoCount.toArrayLike(Buffer, "be", 8)],
+      program.programId
+    );
 
     const tx = await program.rpc.createVideo(
       description,
@@ -114,38 +115,38 @@ const useTiktok = (
           authority: wallet.publicKey,
           ...defaultAccounts,
         },
-      },
-    )
+      }
+    );
 
-    console.log(tx)
+    console.log(tx);
 
-    setDescription('')
-    setVideoUrl('')
-    setNewVideoShow(false)
-  }
+    setDescription("");
+    setVideoUrl("");
+    setNewVideoShow(false);
+  };
   const getComments = async (index, count) => {
-    let commentSigners = []
+    let commentSigners = [];
 
     for (let i = 0; i < count; i++) {
       let [commentSigner] = await anchor.web3.PublicKey.findProgramAddress(
         [
-          utf8.encode('comment'),
-          new BN(index).toArrayLike(Buffer, 'be', 8),
-          new BN(i).toArrayLike(Buffer, 'be', 8),
+          utf8.encode("comment"),
+          new BN(index).toArrayLike(Buffer, "be", 8),
+          new BN(i).toArrayLike(Buffer, "be", 8),
         ],
-        program.programId,
-      )
+        program.programId
+      );
 
-      commentSigners.push(commentSigner)
+      commentSigners.push(commentSigner);
     }
 
     const comments = await program.account.commentAccount.fetchMultiple(
-      commentSigners,
-    )
-    console.log(comments)
-    return comments
-  }
-  return { getTiktoks, likeVideo, createComment, newVideo, getComments }
-}
+      commentSigners
+    );
+    console.log(comments);
+    return comments;
+  };
+  return { getTiktoks, likeVideo, createComment, newVideo, getComments };
+};
 
-export default useTiktok
+export default useTiktok;
